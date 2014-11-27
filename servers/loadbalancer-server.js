7d110349f70c1f5a6b716b9f4e30229da51d43d7
jainikvora <jainik.vora231@gmail.com>
2014-11-26 21:13:46 -0800

    Converted proxy from express to node-http-proxy

--- servers/loadbalancer-server.js
+++ servers/loadbalancer-server.js
@@ -1,33 +1,13 @@
 var httpProxy = require('http-proxy'),
-	express= require('express'),
+	http = require('http'),
 	commonUtil = require('../util/common'),
-	compression = require('compression'),
 	model = require('../model/loadbalancer');
 
 function proxyLoadBalancer() {
 
-var	startTime = "",
-	proxy = httpProxy.createProxyServer(),
-	proxyApp = express();
-
-	proxyApp.use(compression({
-		threshold: model.getGzipThreshold()
-	}))
-
-	proxyApp.use(
-		// function to write response headers - processing time, request id
-		function (req, res, next) {
-		    res.oldWriteHead = res.writeHead;
-
-		  	res.writeHead = function(statusCode, headers) {
-			    res.setHeader('X-HTTP-request-id', commonUtil.generateRequestId());
-			    res.setHeader('X-HTTP-Processing-Time', commonUtil.getExecutionTime(startTime,new Date().getTime()));
-		    res.oldWriteHead(statusCode, headers);
-			}
-
-		    next();
-	 	}, 
-	 	function (req, res) {
+	var	startTime = "",
+		proxy = httpProxy.createProxyServer();
+	http.createServer( function (req, res) {
 			startTime = new Date().getTime();
 			// On each request, get the first location from the list...
 			var target = { target: model.getFirstNode() };
@@ -39,9 +19,13 @@ var	startTime = "",
 			// ...and then the server you just used becomes the last item in the list.
 			model.addNode(target.target);
 		}
-	);
+	).listen(8021);
 
-	proxyApp.listen(8021);
+	// Add processing time and request id after response is received from the target || "proxyRes" event
+	proxy.on('proxyRes', function(proxyRes, req, res) {
+		res.setHeader('X-HTTP-request-id', commonUtil.generateRequestId());
+		res.setHeader('X-HTTP-Processing-Time', commonUtil.getExecutionTime(startTime,new Date().getTime()));
+	})
 
 }
 
