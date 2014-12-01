01e689e1e861db2b2a5a50ca2ea8177410f0f90b
prajaktanaik <pjnaik.1990@gmail.com>
2014-12-01 11:24:43 -0800

    update of mongo in node.js

---
+++ mongodb-nodejs/update.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+
+//connect to the db
+MongoClient.connect("mongodb://team22:japan@ds053320.mongolab.com:53320/loadbalancer",function(err,db){
+	if(!err)
+	 {
+	  console.log("we are connected");
+	  }
+
+  var collection = db.collection('test1');
+  var doc1 = {'hello':'doc1'};
+  var doc2 = {'hello':'doc2'};
+  var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+
+// collection.insert(doc1);
+
+ // collection.insert(doc2, {w:1}, function(err, result) {});
+
+ // collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+
+ // var doc = {mykey:1, fieldtoupdate:1};
+
+    collection.update({'hello':'doc2'}, {$set:{'hello':'prajakta'}}, {w:1}, function(err, result) {});
+
+	//db.createCollection('test',{w:1}, function(err, collection) {});
+});
