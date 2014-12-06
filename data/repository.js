2891923145c7f0863286fb16fa87ee2c6963888a
JvalantPatel <jvalantpatel.patel@gmail.com>
2014-12-05 23:07:52 -0800

    repository changes for healthcheck configuration

--- data/repository.js
+++ data/repository.js
@@ -8,6 +8,23 @@ function AddressRepo() {
 	this.proxyConfig = [];
 
 	this.latency = 0;
+
+    this.healthCkeckConfig ={
+        checkUrl:"/",
+        timeout : 5000,
+        checkInterval : 10000,
+        healthyCount : 3,
+        unhealthyCount : 3
+    }
+
+    this.healthCheckInfo = [
+        /*{
+         host: '127.0.0.1',
+         port: 8085,
+         healthyCount :0,
+         unhealthyCount:0,
+         }*/
+    ];
 }
 
 module.exports = new AddressRepo();
