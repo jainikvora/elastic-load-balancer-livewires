2b020d98df737303bd7e2a7059291d6ca6c9d9ce
JvalantPatel <jvalantpatel.patel@gmail.com>
2014-12-06 19:58:21 -0800

    file added in routes for healthcheck configuration

---
+++ routes/healthcheckroute.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Jvalant on 11/28/2014.
+ */
+
+var express = require('express'),
+    router = express.Router(),
+    model = require('../model/healthcheckmodel');
+
+
+router.route('/')
+    .post(function(req,res){
+        var healthCheckConfig = req.body;
+        if(healthCheckConfig) {
+            model.setHealthCheckConfig(healthCheckConfig);
+            res.status(200).json({'message':'health check configuration updated successfully !!'});
+        } else {
+            res.status(400).json({'message':'Failed'},{'description':'Sorry! Could not process the request. Please check the request data'});
+        }
+    })
+    .get(function(req, res) {
+        res.status(200).json({'message':'success','data':model.getHealthCheckConfig()});
+    });
+
+module.exports = router;
