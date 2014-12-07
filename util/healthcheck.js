2c8f19634e72ea144c1e40918c6463c474e2eab8
JvalantPatel <jvalantpatel.patel@gmail.com>
2014-12-07 11:51:06 -0800

    healthcheck file added for sensing heatbeat of added servers for load balancing

---
+++ util/healthcheck.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Jvalant on 12/2/2014.
+ */
+var http = require('http'),
+    healthCheckModel = require('../model/healthcheckmodel'),
+    loadBalancerModel = require('../model/loadbalancer');
+
+function pingUtility() {
+
+
+    var count =0;
+
+    function loop(){
+        healthCheckModel.getHealthCheckInfo().forEach(function (host) {
+            var target ={
+                host : host.host,
+                port : host.port,
+                path : healthCheckModel.getCheckUrl()
+            };
+            //requestUrl =target.host+":"+target.port+healthCheckModel.getCheckUrl();
+            //console.log(requestUrl);
+            var req = http.get(target, function(res) {
+                //console.log( "Target :"+host.host +""+host.port+" "+'STATUS: ' + res.statusCode);
+
+                if(res.statusCode==200){
+                    if(host.healthyCount<healthCheckModel.getHealthyCount()){
+                        host.healthyCount++;
+                        host.unhealthyCount = 0;
+                    }
+                    else{
+                        if(!loadBalancerModel.checkNodeExists(target))
+                        {
+                            host.unhealthyCount =0;
+                            var targetNew ={
+                                host : target.host,
+                                port : target.port
+                            };
+                            loadBalancerModel.addNode(targetNew);
+                        }
+                    }
+                }
+
+            });
+            req.on('error', function(e) {
+                //console.log('ERROR: for '+host.host +""+host.port + e.message);
+
+                if(host.unhealthyCount<healthCheckModel.getUnHealthyCount()){
+                    host.unhealthyCount++;
+                }
+                else{
+                    if(loadBalancerModel.checkNodeExists(target)){
+                        loadBalancerModel.deleteNode(target);
+                        host.healthyCount =0;
+                    }
+                }
+            });
+        });
+        console.log("Working servers added by load balancer:")
+        console.log(loadBalancerModel.getNodes());
+        console.log("Server status in healthcheck:")
+        console.log(healthCheckModel.getHealthCheckInfo());
+        setTimeout(function () {
+            process.nextTick(function () {
+                loop();
+            });
+        }, healthCheckModel.getCheckInterval());
+    }
+
+    loop();
+
+}
+
+module.exports = new pingUtility();
